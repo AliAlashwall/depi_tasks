c2fb5483595c2b7e178c40df3585594ba4f1c2b8
Ali <alialashwall14@gmail.com>
2024-07-26 17:58:22 +0300

    init

---
+++ details-screen/app/src/main/java/com/example/detailsscreen/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.detailsscreen
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.detailsscreen.presentation.screens.DetailsScreen
+import com.example.detailsscreen.presentation.theme.DetailsScreenTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DetailsScreenTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    DetailsScreen()
+                }
+            }
+        }
+    }
+}
